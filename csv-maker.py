7a7ff40db446ebff77168c3d44377781f67ad2c1
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-02 18:27:59 +0100

    Making of CSV file

--- csv-maker.py
+++ csv-maker.py
@@ -2,15 +2,47 @@ import csv
 from datetime import date
 
 def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, Andre, Teknikkdrill):
-    #Creating filename of todays date
-    current_date = date.today()
-    current_date_string = str(current_date_and_time)
+    #Just for debug
+    I1 = 1000
+    I2 = 2000
+    I3 = 3000
+    I4 = 4000
+    I5 = 5000
+    I6 = 6000
+    I7 = 7000
+    I8 = 8000
+    Tid_svømming = "2:02"
+    Crawl = 1100
+    Egen = 1200
+    Kick = 1300
+    Bein = 1400
+    Arm = 1500
+    Andre = 1600
+    Teknikkdrill = 1700
 
-    #Creating CSV file
-    with open(current_date_string, 'wb') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow([
+    #List of data
+    data = [
+        date.today(),
+        I1,
+        I2,
+        I3,
+        I4,
+        I5,
+        I6,
+        I7,
+        I8,
+        Tid,
+        Crawl,
+        Egen,
+        Kick,
+        Bein,
+        Arm,
+        Andre,
+        Teknikkdrill
+        ]
+
+    #List of names
+    header = [
         'Dato',
         'I1',
         'I2',
@@ -27,24 +59,14 @@ def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, 
         'Bein',
         'Arm',
         'Andre',
-        'Teknikkdrill'
-        ])
-        filewriter.writerow([
-        current_date_string,
-        I1,
-        I2,
-        I3,
-        I4,
-        I5,
-        I6,
-        I7,
-        I8,
-        Tid svømming,
-        Crawl,
-        Egen,
-        Kick,
-        Bein,
-        Arm,
-        Andre,
-        Teknikkdrill
-        ])
+        'Teknikkdrill']
+
+    #Creating filename of todays date
+    current_date = date.today()
+    current_date_string = str(current_date)+ '.csv'
+
+    #Creating CSV file
+    with open(current_date_string, 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(header)
+        filewriter.writerow(data)
