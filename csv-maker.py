d37b44e542d7ddd6febe3521d4347f4d360edf7f
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-02 16:01:29 +0100

    Making CSV-making a function, and added variables

--- csv-maker.py
+++ csv-maker.py
@@ -1,31 +1,50 @@
 import csv
 from datetime import date
 
-#Creating filename of todays date
-current_date_and_time = date.today()
-current_date_and_time_string = str(current_date_and_time)
+def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, Andre, Teknikkdrill):
+    #Creating filename of todays date
+    current_date = date.today()
+    current_date_string = str(current_date_and_time)
 
-#Creating CSV file
-with open(current_date_and_time_string, 'wb') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow([
-    'Dato',
-    'I1',
-    'I2',
-    'I3',
-    'I4',
-    'I5',
-    'I6',
-    'I7',
-    'I8',
-    'Tid svømming',
-    'Tid',
-    'Crawl',
-    'Egen',
-    'Kick',
-    'Bein',
-    'Arm',
-    'Teknikkdrill'
-    ])
-    filewriter.writerow(['Derek', 'Software Developer'])
+    #Creating CSV file
+    with open(current_date_string, 'wb') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow([
+        'Dato',
+        'I1',
+        'I2',
+        'I3',
+        'I4',
+        'I5',
+        'I6',
+        'I7',
+        'I8',
+        'Tid svømming',
+        'Crawl',
+        'Egen',
+        'Kick',
+        'Bein',
+        'Arm',
+        'Andre',
+        'Teknikkdrill'
+        ])
+        filewriter.writerow([
+        current_date_string,
+        I1,
+        I2,
+        I3,
+        I4,
+        I5,
+        I6,
+        I7,
+        I8,
+        Tid svømming,
+        Crawl,
+        Egen,
+        Kick,
+        Bein,
+        Arm,
+        Andre,
+        Teknikkdrill
+        ])
