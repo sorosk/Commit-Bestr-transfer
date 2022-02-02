bc858a58ea4a02a63ab718cb43f5157eccae2a8b
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-02 19:06:28 +0100

    Styling issue

--- csv_maker.py
+++ csv_maker.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import date
 
+
 def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, Andre, Teknikkdrill):
     # List of names
     header = [
@@ -43,12 +44,12 @@ def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, 
         Teknikkdrill
         ]
 
-    #Creating filename of todays date
+    # Creating filename of todays date
     current_date = date.today()
-    current_date_string = str(current_date)+ '.csv'
+    current_date_string = str(current_date) + '.csv'
 
-    #Creating CSV file
-    with open(current_date_string, 'w', newline='') as csvfile: #The CSV file name is todays date
+    # Creating CSV file
+    with open(current_date_string, 'w', newline='') as csvfile:  # The CSV file name is todays date
         filewriter = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(header) #Row of names/headers
-        filewriter.writerow(data) #Row of data
+        filewriter.writerow(header)  # Row of names/headers
+        filewriter.writerow(data)  # Row of data
