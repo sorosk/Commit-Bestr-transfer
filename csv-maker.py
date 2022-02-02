f5d3c7c146d94d4ce95bfe2290d61bb16060c6b9
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-02 18:33:16 +0100

    Integrated csv-maker into main.py

--- csv-maker.py
+++ csv-maker.py
@@ -2,23 +2,25 @@ import csv
 from datetime import date
 
 def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, Andre, Teknikkdrill):
-    #Just for debug
-    I1 = 1000
-    I2 = 2000
-    I3 = 3000
-    I4 = 4000
-    I5 = 5000
-    I6 = 6000
-    I7 = 7000
-    I8 = 8000
-    Tid_svømming = "2:02"
-    Crawl = 1100
-    Egen = 1200
-    Kick = 1300
-    Bein = 1400
-    Arm = 1500
-    Andre = 1600
-    Teknikkdrill = 1700
+    #List of names
+    header = [
+        'Dato',
+        'I1',
+        'I2',
+        'I3',
+        'I4',
+        'I5',
+        'I6',
+        'I7',
+        'I8',
+        'Tid svømming',
+        'Crawl',
+        'Egen',
+        'Kick',
+        'Bein',
+        'Arm',
+        'Andre',
+        'Teknikkdrill']
 
     #List of data
     data = [
@@ -41,32 +43,12 @@ def makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, 
         Teknikkdrill
         ]
 
-    #List of names
-    header = [
-        'Dato',
-        'I1',
-        'I2',
-        'I3',
-        'I4',
-        'I5',
-        'I6',
-        'I7',
-        'I8',
-        'Tid svømming',
-        'Crawl',
-        'Egen',
-        'Kick',
-        'Bein',
-        'Arm',
-        'Andre',
-        'Teknikkdrill']
-
     #Creating filename of todays date
     current_date = date.today()
     current_date_string = str(current_date)+ '.csv'
 
     #Creating CSV file
-    with open(current_date_string, 'w', newline='') as csvfile:
+    with open(current_date_string, 'w', newline='') as csvfile: #The CSV file name is todays date
         filewriter = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(header)
-        filewriter.writerow(data)
+        filewriter.writerow(header) #Row of names/headers
+        filewriter.writerow(data) #Row of data
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import os
+import csv-maker
 
-
+### Username for BESTR ###
 username = os.environ['username']
 pwd = os.environ['pwd']
+
+### CSV writing ###
+I1 = 1000
+I2 = 2000
+I3 = 3000
+I4 = 4000
+I5 = 5000
+I6 = 6000
+I7 = 7000
+I8 = 8000
+Tid_svømming = "2:02"
+Crawl = 1100
+Egen = 1200
+Kick = 1300
+Bein = 1400
+Arm = 1500
+Andre = 1600
+Teknikkdrill = 1700
+
+csv-maker.makefile(I1, I2, I3, I4, I5, I6, I7, I8, Tid, Crawl, Egen, Kick, Bein, Arm, Andre, Teknikkdrill)
