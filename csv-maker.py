07af556f7084a3f3f34a86c0f313ba4f18fae914
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-02 15:26:08 +0100

    Naming CSV file to current date and time

--- csv-maker.py
+++ csv-maker.py
@@ -1,10 +1,30 @@
 import csv
 import datetime
 
-with open('datetime.date()', 'wb') as csvfile:
+
+current_date_and_time = datetime.datetime.now()
+current_date_and_time_string = str(current_date_and_time)
+
+with open(current_date_and_time_string, 'wb') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['Name', 'Profession'])
+    filewriter.writerow([
+    'Dato', 
+    'I1',
+    'I2',
+    'I3',
+    'I4',
+    'I5',
+    'I6',
+    'I7',
+    'I8',
+    'Tid svømming',
+    'Tid',
+    'Crawl',
+    'Egen',
+    'Kick',
+    'Bein',
+    'Arm',
+    'Teknikkdrill'
+    ])
     filewriter.writerow(['Derek', 'Software Developer'])
-    filewriter.writerow(['Steve', 'Software Developer'])
-    filewriter.writerow(['Paul', 'Manager'])
