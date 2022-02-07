8794709a52295fdb4b9ce84db4b26e65d4b8f028
Daddahu <70315095+sorosk@users.noreply.github.com>
2022-02-07 11:06:03 +0100

    Auth format
    Signed-off-by: Daddahu <70315095+sorosk@users.noreply.github.com>

--- webcrawler.py
+++ webcrawler.py
@@ -2,18 +2,6 @@ import requests
 import json
 
 def login(username, password):
-    cookies = {
-        'locale': 'nb-NO',
-        '_ga': 'GA1.1.940420002.1641239911',
-        '_hjSessionUser_2749362': 'eyJpZCI6IjI1N2ZkNTQxLTQ0MTgtNTQzNi1iZjNkLTA5ZmFkM2UwYTUxNCIsImNyZWF0ZWQiOjE2NDEyMzk5MTA5ODUsImV4aXN0aW5nIjp0cnVlfQ==',
-        '_hjSession_2749362': 'eyJpZCI6IjlkZTU4YWJmLTlhMWYtNGRiOC05NmIzLWJkMmE0OTU2MWQyNiIsImNyZWF0ZWQiOjE2NDQwNTE3MzEwNDEsImluU2FtcGxlIjpmYWxzZX0=',
-        '_hjAbsoluteSessionInProgress': '0',
-        '_hjIncludedInPageviewSample': '1',
-        '_hjIncludedInSessionSample': '1',
-        'authentication': 'CfDJ8JCnWWnWZg5HtInfGoQkTiuPBFdw8OO8c3UetUcvfCEJmX79NOK9F7xkPr6ejaE7QjzyMOzwUaz9MF6UMsq3eCTXEv2VNHh40TvGgzYOluWP61qq4p76WHS1QNCgFcLfCheV_OqHvhzQMolbqQXFhnrRj9B4rWaYFMG4neieIB4YGYD_9o2V4WcrJs_0m-eM7gZi_5f_XdR-twJGhSbxsQhZiJ1OlQYRnJ0cGrTq1181v6GVRYxH9bqEzJ6M0yHU99S5Xyo5d01sf93ZggvC9qY9igZd-LEqG6cm35PvmlRd1qgFKuWkyQx46KhV3OnpYlNus56kCgMJdoZo5IBM7_N77mK0uhWBK-p_r38ET8CX1ADHhKilBARw9UATIveRTGWFEtIqfX2Ylgvy-kErGwrWXiLlPS7vj3aKe3qO0GrXMpgxsxM3Cu7V9shpyS4aJP6bazxSUVJJEwAebNOa7FgNeNDUQCyg3zJN4ILhNofcXqgimttlKQ5Ui3m0cQOmHRZpY5HzbHY1ZVbYmt_5-DU',
-        '_ga_VNK9X5XJ3H': 'GS1.1.1644051730.46.1.1644054672.0',
-    }
-
     headers = {
         'Connection': 'keep-alive',
         'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
@@ -33,23 +21,12 @@ def login(username, password):
 
     pre_data = '"password":"{}","persistent":false,"username":"{}"'.format(password, username)
     data = '{'+pre_data+'}'
-    response = requests.post('https://www.bestr.no/api/membership/accounts/login', headers=headers, cookies=cookies,
+    response = requests.post('https://www.bestr.no/api/membership/accounts/login', headers=headers,
                              data=data)
     print(response)
+    global cookies
+    cookies = response.cookies.get_dict()
     return requests
-
-
-cookies = {
-    'locale': 'nb-NO',
-    '_ga': 'GA1.1.940420002.1641239911',
-    '_hjSessionUser_2749362': 'eyJpZCI6IjI1N2ZkNTQxLTQ0MTgtNTQzNi1iZjNkLTA5ZmFkM2UwYTUxNCIsImNyZWF0ZWQiOjE2NDEyMzk5MTA5ODUsImV4aXN0aW5nIjp0cnVlfQ==',
-    'authentication': 'CfDJ8JCnWWnWZg5HtInfGoQkTiv5aC84EIZ2FLcMKtHpk7ShJk43d4-LAYetMc-KgPw5PArXyPWEX2ACwfRqYlRyhFYopXBBydOSWyQ5YalyPcmRmCaA4zcEwCz3P--OaSMDx0pa_nJUbgBP5qjDVC2oX_cQos3MoVp4sQMEUH9ANAvxe0oKJwmNH_i2YqzZ5eUxXrnYkDgie1syE3nLLiCA7Dn5LojcR7299X2TRlaOUgKZGK6SDm_wjJZYi9KS7viX4FND1HS_sGa_mq2I3Gg2zdl7XcfelEKrKzqgo0mfhzYrJJIlUTOh2oTgKfmFdUdDxToc3zq_broMIWm4uoCrlX5ZR2cPUyoi8Yj4HkDLrPCQHKanB9Mm1YxYQDA1yn5UqmRC8Naanf3sNzhYyIMEsauMpLsmSvmfEkfxBgVxibnYhXRSyFDmpv03taXJKTVILF6jgiigIBMIYf3RrqtnsTubveBJMMJmQ_Y0Ebdgh2syOWme53dxX0H-FcIDTbPQZDgLCwlD8SY1HFuwsB5IoYY',
-    '_hjSession_2749362': 'eyJpZCI6Ijg1M2FjNTFlLWQxOWYtNGI5Mi1iMDJhLWJiYzM3NjIwMGQ3OCIsImNyZWF0ZWQiOjE2NDQwNjc0MjUxNDYsImluU2FtcGxlIjp0cnVlfQ==',
-    '_hjAbsoluteSessionInProgress': '0',
-    '_hjIncludedInPageviewSample': '1',
-    '_hjIncludedInSessionSample': '0',
-    '_ga_VNK9X5XJ3H': 'GS1.1.1644067424.48.1.1644068790.0',
-}
 
 headers = {
     'Connection': 'keep-alive',
@@ -76,7 +53,7 @@ with open("2022-02-04.csv", 'r') as f:
    print(text2)
 
 pre_data = '"content":"{}","sportId":"08d60015402ff40ddac564839c40516d"'.format(text2)
-data = '{'+pre_data+'}'
+data = '{'+pre_data+'}'.replace(("\n", "\\n"))
 print(data)
 r = session.post('https://www.bestr.no/api/migration/import/csv', headers=headers, cookies=cookies, data=data)
 
